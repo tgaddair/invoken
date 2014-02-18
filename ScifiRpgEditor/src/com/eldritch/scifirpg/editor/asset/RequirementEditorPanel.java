74e34ccfee1b3f0eece543daea74e7c4e80b505c
Travis Addair <tgaddair@gmail.com>
2014-02-17 23:24:32 -0800

    Saving now adds a row to the table model

---
+++ ScifiRpgEditor/src/com/eldritch/scifirpg/editor/asset/RequirementEditorPanel.java
@@ -0,0 +1,66 @@
+package com.eldritch.scifirpg.editor.asset;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import com.eldritch.scifirpg.editor.tables.RequirementTable;
+import com.eldritch.scifirpg.proto.Augmentations.Augmentation.Requirement;
+import com.eldritch.scifirpg.proto.Disciplines.Discipline;
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+
+public class RequirementEditorPanel extends JPanel implements ActionListener {
+	private static final long serialVersionUID = 1L;
+
+	private final RequirementTable owner;
+	private final JComboBox<Discipline> disciplineBox = new JComboBox<Discipline>(
+			Discipline.values());
+	private final JTextField valueField = new JTextField();
+	private final JTextField slotsField = new JTextField();
+
+	public RequirementEditorPanel(RequirementTable owner) {
+		super(new BorderLayout());
+		this.owner = owner;
+
+		DefaultFormBuilder builder = new DefaultFormBuilder(new FormLayout(""));
+		builder.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		builder.appendColumn("right:pref");
+		builder.appendColumn("3dlu");
+		builder.appendColumn("fill:max(pref; 100px)");
+
+		builder.append("Discipline:", disciplineBox);
+		builder.nextLine();
+
+		builder.append("Value:", valueField);
+		builder.nextLine();
+
+		builder.append("Slots:", slotsField);
+		builder.nextLine();
+
+		JButton saveButton = new JButton("Save");
+		saveButton.addActionListener(this);
+		builder.append(saveButton);
+		builder.nextLine();
+
+		add(builder.getPanel());
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Discipline discipline = (Discipline) disciplineBox.getSelectedItem();
+		int value = Integer.parseInt(valueField.getText());
+		int slots = Integer.parseInt(slotsField.getText());
+		Requirement req = Requirement.newBuilder().setDiscipline(discipline)
+				.setValue(value)
+				.setSlots(slots)
+				.build();
+		owner.addAsset(req);
+	}
+}
---
+++ ScifiRpgEditor/src/com/eldritch/scifirpg/editor/tables/RequirementTable.java
@@ -0,0 +1,32 @@
+package com.eldritch.scifirpg.editor.tables;
+
+import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
+
+import com.eldritch.scifirpg.editor.asset.RequirementEditorPanel;
+import com.eldritch.scifirpg.proto.Augmentations.Augmentation.Requirement;
+
+public class RequirementTable extends AssetTable {
+	private static final long serialVersionUID = 1L;
+	private static final String[] COLUMN_NAMES = { 
+		"Discipline", "Value", "Slots" };
+	
+	public RequirementTable() {
+		super(COLUMN_NAMES);
+	}
+
+	@Override
+	protected JPanel getEditorPanel() {
+		return new RequirementEditorPanel(this);
+	}
+	
+	@Override
+	protected String getAssetName() {
+		return "Requirement";
+	}
+	
+	public void addAsset(Requirement req) {
+		DefaultTableModel model = (DefaultTableModel) getModel();
+		model.addRow(new Object[]{req.getDiscipline(), req.getValue(), req.getSlots()});
+	}
+}
