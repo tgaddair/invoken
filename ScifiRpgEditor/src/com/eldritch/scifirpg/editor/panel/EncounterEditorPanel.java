792d413c6e69cd4ac2a7987d8aebfac0b8f59e88
Travis Addair <tgaddair@gmail.com>
2014-02-23 11:30:05 -0800

    Added static layout

--- ScifiRpgEditor/src/com/eldritch/scifirpg/editor/panel/EncounterEditorPanel.java
+++ ScifiRpgEditor/src/com/eldritch/scifirpg/editor/panel/EncounterEditorPanel.java
@@ -1,16 +1,22 @@
 package com.eldritch.scifirpg.editor.panel;
 
+import java.awt.CardLayout;
 import java.awt.Dimension;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import com.eldritch.scifirpg.editor.AssetTablePanel;
 import com.eldritch.scifirpg.editor.tables.EncounterTable;
+import com.eldritch.scifirpg.editor.tables.OutcomeTable;
 import com.eldritch.scifirpg.editor.tables.PrerequisiteTable;
 import com.eldritch.scifirpg.proto.Locations.Encounter;
 import com.eldritch.scifirpg.proto.Locations.Encounter.Type;
@@ -19,7 +25,7 @@ import com.google.common.base.Optional;
 import com.jgoodies.forms.builder.DefaultFormBuilder;
 import com.jgoodies.forms.layout.FormLayout;
 
-public class EncounterEditorPanel extends AssetEditorPanel<Encounter, EncounterTable> {
+public class EncounterEditorPanel extends AssetEditorPanel<Encounter, EncounterTable> implements ItemListener {
 	private static final long serialVersionUID = 1L;
 
 	private final JTextField idField = new JTextField();
@@ -28,16 +34,15 @@ public class EncounterEditorPanel extends AssetEditorPanel<Encounter, EncounterT
 	private final JTextField weightField = new JTextField();
 	private final JCheckBox uniqueCheck = new JCheckBox();
 	private final PrerequisiteTable prereqTable = new PrerequisiteTable();
+	
+	// Different cards for different types
+	private final JPanel cards;
+	private final StaticEncounterPanel staticPanel = new StaticEncounterPanel();
 
 	public EncounterEditorPanel(EncounterTable owner, JFrame frame, Optional<Encounter> prev) {
 		super(owner, frame, prev);
 
-		DefaultFormBuilder builder = new DefaultFormBuilder(new FormLayout(""));
-		builder.border(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-		builder.appendColumn("right:pref");
-		builder.appendColumn("3dlu");
-		builder.appendColumn("fill:max(pref; 100px)");
-		
+		DefaultFormBuilder builder = createFormBuilder();
 		titleField.addActionListener(new NameTypedListener(idField));
 		builder.append("Title:", titleField);
 		builder.nextLine();
@@ -57,6 +62,13 @@ public class EncounterEditorPanel extends AssetEditorPanel<Encounter, EncounterT
 		builder.appendRow("fill:120dlu");
 		builder.append("Prerequisites:", new AssetTablePanel(prereqTable));
 		builder.nextLine();
+		
+		cards = new JPanel(new CardLayout());
+		cards.add(staticPanel, Type.STATIC.name());
+		
+		builder.appendRow("fill:p:grow");
+		builder.append("Parameters:", cards);
+		builder.nextLine();
 
 		JButton saveButton = new JButton("Save");
 		saveButton.addActionListener(this);
@@ -75,12 +87,46 @@ public class EncounterEditorPanel extends AssetEditorPanel<Encounter, EncounterT
 		}
 
 		add(builder.getPanel());
-		setPreferredSize(new Dimension(650, 750));
+		setPreferredSize(new Dimension(800, 750));
 	}
 
 	@Override
 	public Encounter createAsset() {
-		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent ev) {
+		CardLayout cl = (CardLayout) cards.getLayout();
+		Type t = (Type) ev.getItem();
+        cl.show(cards, t.name());
+	}
+	
+	private class StaticEncounterPanel extends JPanel {
+		private static final long serialVersionUID = 1L;
+		
+		private final JTextArea descriptionField = createArea(true, 30, new Dimension(100, 100));
+		private final OutcomeTable outcomeTable = new OutcomeTable();
+		
+		public StaticEncounterPanel() {
+			DefaultFormBuilder builder = createFormBuilder();
+			builder.append("Description:", descriptionField);
+			builder.nextLine();
+			
+			builder.appendRow("fill:120dlu");
+			builder.append("Outcomes:", new AssetTablePanel(outcomeTable));
+			builder.nextLine();
+			
+			add(builder.getPanel());
+		}
+	}
+	
+	private static DefaultFormBuilder createFormBuilder() {
+		DefaultFormBuilder builder = new DefaultFormBuilder(new FormLayout(""));
+		builder.border(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		builder.appendColumn("right:pref");
+		builder.appendColumn("3dlu");
+		builder.appendColumn("fill:max(pref; 100px)");
+		return builder;
 	}
 }
