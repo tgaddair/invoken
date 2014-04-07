691e05766c83ec25f8de3af7ceeacf5d908c9e45
Travis Addair <tgaddair@gmail.com>
2014-04-06 18:54:19 -0700

    Improved following

--- InvokenGame/core/src/com/eldritch/invoken/actor/ai/FollowRoutine.java
+++ InvokenGame/core/src/com/eldritch/invoken/actor/ai/FollowRoutine.java
@@ -1,6 +1,8 @@
 package com.eldritch.invoken.actor.ai;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
+import com.eldritch.invoken.InvokenGame;
 import com.eldritch.invoken.actor.Npc;
 
 public class FollowRoutine implements Routine {
@@ -9,19 +11,23 @@ public class FollowRoutine implements Routine {
 	/** cannot stray too far from the origin location, or we're wandering, not patrolling */
 	private final int maxDistance; // from origin
 	private final float maxVelocity;
+	private final float duration; // before we update our destination
+	private float elapsed;
 	
 	/** how long we move in a single direction before turning */ 
 	private float xVelocity;
 	private float yVelocity;
 	
 	public FollowRoutine(Npc npc) {
-		this(npc, npc.getMaxVelocity() / 4, 5);
+		this(npc, npc.getMaxVelocity() * 0.8f, 3, 0.1f);
 	}
 	
-	public FollowRoutine(Npc npc, float maxVelocity, int maxDistance) {
+	public FollowRoutine(Npc npc, float maxVelocity, int maxDistance, float duration) {
 		this.npc = npc;
 		this.maxDistance = maxDistance;
 		this.maxVelocity = maxVelocity;
+		this.duration = duration;
+		elapsed = duration;
 	}
 	
 	@Override
@@ -36,14 +42,26 @@ public class FollowRoutine implements Routine {
 	
 	@Override
 	public void takeAction(float delta) {
-		if (hasStrayed()) {
-			// move towards the origin to get us back on track
-			Vector2 origin = getTarget();
+		elapsed += delta;
+		if (!hasStrayed()) {
+			xVelocity = yVelocity = 0;
+		} else {
+			// roughly move towards the origin to get us back on track
 			Vector2 position = npc.getPosition();
-			float dx = origin.x - position.x;
-			float dy = origin.y - position.y;
-			xVelocity = Math.signum(dx) * maxVelocity;
-			yVelocity = Math.signum(dy) * maxVelocity;
+			Vector2 target = getTarget();
+			float dx = target.x - position.x;
+			float dy = target.y - position.y;
+			
+			xVelocity = yVelocity = 0;
+			if (Math.abs(Math.abs(dx) - Math.abs(dy)) < 0.1) {
+				// prevents flickering when moving along the diagonal
+				xVelocity = Math.signum(dx) * maxVelocity;
+				yVelocity = Math.signum(dy) * maxVelocity;
+			} else if (Math.abs(dx) > Math.abs(dy)) {
+				xVelocity = Math.signum(dx) * maxVelocity;
+			} else {
+				yVelocity = Math.signum(dy) * maxVelocity;
+			}
 		}
 		
 		resetVelocity();
@@ -53,7 +71,7 @@ public class FollowRoutine implements Routine {
 		npc.setVelocity(xVelocity, yVelocity);
 	}
 	
-	private Vector2 getTarget() {
+	public Vector2 getTarget() {
 		return npc.getFollowed().getPosition();
 	}
 	
