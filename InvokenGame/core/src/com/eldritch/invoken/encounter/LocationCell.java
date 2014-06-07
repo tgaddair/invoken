d69ed7d833a319e5c708443452df21eb01d59b15
Travis Addair <tgaddair@gmail.com>
2014-06-07 13:42:44 -0700

    Added LocationCell

---
+++ InvokenGame/core/src/com/eldritch/invoken/encounter/LocationCell.java
@@ -0,0 +1,23 @@
+package com.eldritch.invoken.encounter;
+
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
+import com.eldritch.invoken.encounter.layer.LocationLayer;
+
+public class LocationCell extends Cell {
+    private final NaturalVector2 position;
+    private final LocationLayer layer;
+    
+    public LocationCell(NaturalVector2 position, LocationLayer layer) {
+        this.position = position;
+        this.layer = layer;
+    }
+    
+    @Override
+    public TiledMapTile getTile () {
+        if (layer.isVisible(position.x, position.y)) {
+            return super.getTile();
+        }
+        return null;
+    }
+}
