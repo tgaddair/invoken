9679c78ae40a1d1e70b0aeaa7792a4791194d6c3
Travis Addair <tgaddair@gmail.com>
2014-06-15 10:52:32 -0700

    Improved equipment previews in inventory

--- InvokenGame/core/src/com/eldritch/invoken/ui/InventoryMenu.java
+++ InvokenGame/core/src/com/eldritch/invoken/ui/InventoryMenu.java
@@ -15,85 +15,90 @@ import com.eldritch.invoken.screens.AbstractScreen;
 import com.eldritch.invoken.util.DefaultInputListener;
 
 public class InventoryMenu {
-	private final Window window;
-	private final Table table;
-	private final Skin skin;
-	private final Player player;
-	
-	public InventoryMenu(Player player, Skin skin) {
-		this.player = player;
-		this.skin = skin;
+    private final Window window;
+    private final Table table;
+    private final Skin skin;
+    private final Player player;
+    private final SplitPane splitPane;
 
-	    table = new Table(skin);
-		table.top();
-		
-		ScrollPane scroll = new ScrollPane(table, skin);
-		ScrollPane playerView = new ScrollPane(new Image(player.getPortrait()));
-		SplitPane splitPane = new SplitPane(playerView, scroll, false, skin, "default-horizontal");
-		
-		window = new Window("Inventory", skin);
-		window.setHeight(AbstractScreen.MENU_VIEWPORT_HEIGHT - 100);
-		window.setWidth(AbstractScreen.MENU_VIEWPORT_WIDTH - 100);
-		window.setPosition(
-              AbstractScreen.MENU_VIEWPORT_WIDTH / 2 - window.getWidth() / 2,
-              AbstractScreen.MENU_VIEWPORT_HEIGHT / 2 - window.getHeight() / 2);
-		window.center();
-		
-		TextButton closeButton = new TextButton("X", skin);
-		closeButton.addListener(new DefaultInputListener() {
+    public InventoryMenu(Player player, Skin skin) {
+        this.player = player;
+        this.skin = skin;
+
+        table = new Table(skin);
+        table.top();
+
+        ScrollPane scroll = new ScrollPane(table, skin);
+        splitPane = new SplitPane(getPlayerView(), scroll, false, skin, "default-horizontal");
+
+        window = new Window("Inventory", skin);
+        window.setHeight(AbstractScreen.MENU_VIEWPORT_HEIGHT - 100);
+        window.setWidth(AbstractScreen.MENU_VIEWPORT_WIDTH - 100);
+        window.setPosition(AbstractScreen.MENU_VIEWPORT_WIDTH / 2 - window.getWidth() / 2,
+                AbstractScreen.MENU_VIEWPORT_HEIGHT / 2 - window.getHeight() / 2);
+        window.center();
+
+        TextButton closeButton = new TextButton("X", skin);
+        closeButton.addListener(new DefaultInputListener() {
             @Override
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 show(false);
             }
         });
-		
+
         window.getButtonTable().add(closeButton).height(window.getPadTop());
         window.row().fill().expandX();
         window.add(splitPane).expand().fill();
         window.setVisible(false);
-	}
-	
-	public Window getTable() {
-		return window;
-	}
-	
-	public void toggle() {
-		show(!window.isVisible());
-	}
-	
-	public void show(boolean visible) {
-	    window.setVisible(visible);
-		if (visible) {
-			refresh();
-		}
-	}
-	
-	public void refresh() {
-		table.clear();
-		for (Inventory.ItemState item : player.getInfo().getInventory().getItems()) {
-			addItemButton(item);
-		}
-	}
-	
-	private void addItemButton(Inventory.ItemState itemState) {
-		final Item item = itemState.getItem();
-		final TextButton itemButton = new TextButton(getText(item, itemState.getCount()), skin);
-		itemButton.addListener(new DefaultInputListener() {
-			@Override
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-			    Inventory inventory = player.getInfo().getInventory();
-			    if (item.isEquipped(inventory)) {
-			        inventory.unequip(item);
-			    } else {
-			        inventory.equip(item);
-			    }
-			}
-		});
-		table.row();
-		table.add(itemButton).expandX().fillX().padLeft(5).padRight(5).padBottom(5).padTop(5);
-	}
-	
-	private String getText(Item item, int count) {
-		return String.format("%s (%d)", item.getName(), count);
-	}
+    }
+
+    public Window getTable() {
+        return window;
+    }
+
+    public void toggle() {
+        show(!window.isVisible());
+    }
+
+    public void show(boolean visible) {
+        window.setVisible(visible);
+        if (visible) {
+            refresh();
+        }
+    }
+
+    public void refresh() {
+        table.clear();
+        for (Inventory.ItemState item : player.getInfo().getInventory().getItems()) {
+            addItemButton(item);
+        }
+        splitPane.setFirstWidget(getPlayerView());
+    }
+
+    private void addItemButton(Inventory.ItemState itemState) {
+        final Item item = itemState.getItem();
+        final TextButton itemButton = new TextButton(getText(item, itemState.getCount()), skin);
+        itemButton.addListener(new DefaultInputListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                Inventory inventory = player.getInfo().getInventory();
+                if (item.isEquipped(inventory)) {
+                    inventory.unequip(item);
+                } else {
+                    inventory.equip(item);
+                }
+                splitPane.setFirstWidget(getPlayerView());
+            }
+        });
+        table.row();
+        table.add(itemButton).expandX().fillX().padLeft(5).padRight(5).padBottom(5).padTop(5);
+    }
+
+    private ScrollPane getPlayerView() {
+        return new ScrollPane(new Image(player.getPortrait()));
+    }
+
+    private String getText(Item item, int count) {
+        return String.format("%s (%d)", item.getName(), count);
+    }
 }
