e0b89e75755c75154afb9cc1d55ae84e301f8cd2
Travis Addair <tgaddair@gmail.com>
2014-12-24 10:06:22 -0800

    Refactored Cracking into ToggledEffect

--- InvokenGame/core/src/com/eldritch/invoken/effects/Cracking.java
+++ InvokenGame/core/src/com/eldritch/invoken/effects/Cracking.java
@@ -1,49 +1,19 @@
 package com.eldritch.invoken.effects;
 
-import com.eldritch.invoken.actor.aug.Augmentation;
 import com.eldritch.invoken.actor.aug.Crack;
 import com.eldritch.invoken.actor.type.Agent;
 
-public class Cracking extends BasicEffect {
-    private final Augmentation aug;
-    private final int cost;
-    private float elapsed = 0;
-    private boolean finished = false;
-	
-	public Cracking(Agent target, Augmentation aug, int cost) {
-	    super(target);
-	    this.aug = aug;
-	    this.cost = cost;
+public class Cracking extends ToggledEffect<Crack> {
+	public Cracking(Agent target, Crack aug, int cost) {
+	    super(target, aug, Crack.class, cost);
 	}
 
 	@Override
-	public boolean isFinished() {
-		return finished || (isApplied() && !target.isToggled(Crack.class));
+	protected void afterApply() {
 	}
 
 	@Override
-	public void dispel() {
-	    target.toggleOff(Crack.class);
-	    target.getInfo().getAugmentations().removeActiveAugmentation(aug);
-	    target.resetCamera();
+	protected void afterDispel() {
+		target.resetCamera();
 	}
-	
-	@Override
-    protected void doApply() {
-	    target.toggleOn(Crack.class);
-    }
-
-    @Override
-    protected void update(float delta) {
-        elapsed += delta;
-        if (elapsed > 1) {
-            // drains continuously while active
-            if (cost <= target.getInfo().getEnergy()) {
-                target.getInfo().expend(cost);
-            } else {
-                finished = true;
-            }
-            elapsed = 0;
-        }
-    }
 }
---
+++ InvokenGame/core/src/com/eldritch/invoken/effects/ToggledEffect.java
@@ -0,0 +1,55 @@
+package com.eldritch.invoken.effects;
+
+import com.eldritch.invoken.actor.aug.Augmentation;
+import com.eldritch.invoken.actor.type.Agent;
+
+public abstract class ToggledEffect<T extends Augmentation> extends BasicEffect {
+    private final T aug;
+    private final Class<T> toggle;
+    private final int cost;
+    private float elapsed = 0;
+    private boolean finished = false;
+	
+	public ToggledEffect(Agent target, T aug, Class<T> toggle, int cost) {
+	    super(target);
+	    this.aug = aug;
+	    this.toggle = toggle;
+	    this.cost = cost;
+	}
+
+	@Override
+	public boolean isFinished() {
+		return finished || (isApplied() && !target.isToggled(toggle));
+	}
+
+	@Override
+	public void dispel() {
+	    target.toggleOff(toggle);
+	    target.getInfo().getAugmentations().removeActiveAugmentation(aug);
+	    afterDispel();
+	}
+	
+	@Override
+    protected void doApply() {
+	    target.toggleOn(toggle);
+	    afterApply();
+    }
+
+    @Override
+    protected void update(float delta) {
+        elapsed += delta;
+        if (elapsed > 1) {
+            // drains continuously while active
+            if (cost <= target.getInfo().getEnergy()) {
+                target.getInfo().expend(cost);
+            } else {
+                finished = true;
+            }
+            elapsed = 0;
+        }
+    }
+    
+    protected abstract void afterApply();
+    
+    protected abstract void afterDispel();
+}
