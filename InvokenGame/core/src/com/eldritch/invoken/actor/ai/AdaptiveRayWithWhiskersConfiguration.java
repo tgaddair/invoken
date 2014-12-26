eff941bba292df10abf6d92484982fe6dfbdbb45
Travis Addair <tgaddair@gmail.com>
2014-12-26 12:14:06 -0800

    Tweaked the ray length to be adapative as well

--- InvokenGame/core/src/com/eldritch/invoken/actor/ai/AdaptiveRayWithWhiskersConfiguration.java
+++ InvokenGame/core/src/com/eldritch/invoken/actor/ai/AdaptiveRayWithWhiskersConfiguration.java
@@ -6,11 +6,13 @@ import com.badlogic.gdx.ai.steer.utils.rays.CentralRayWithWhiskersConfiguration;
 import com.badlogic.gdx.math.Vector;
 
 public class AdaptiveRayWithWhiskersConfiguration<T extends Vector<T>> extends CentralRayWithWhiskersConfiguration<T> {
+	private final float maxRayLength;
 	private final float maxWhiskerAngle;
 
 	public AdaptiveRayWithWhiskersConfiguration(Steerable<T> owner,
-			float rayLength, float whiskerLength, float maxWhiskerAngle) {
-		super(owner, rayLength, whiskerLength, maxWhiskerAngle);
+			float maxRayLength, float whiskerLength, float maxWhiskerAngle) {
+		super(owner, maxRayLength, whiskerLength, maxWhiskerAngle);
+		this.maxRayLength = maxRayLength;
 		this.maxWhiskerAngle = maxWhiskerAngle;
 	}
 	
@@ -19,8 +21,13 @@ public class AdaptiveRayWithWhiskersConfiguration<T extends Vector<T>> extends C
 		// Scale the whisker angle down to 0 as the owner's linear velocity approaches the max.
 		float velocity = owner.getLinearVelocity().len2();
 		float maxVelocity = owner.getMaxLinearSpeed() * owner.getMaxLinearSpeed();
+		
 		float angle = ((maxVelocity - velocity) / maxVelocity) * maxWhiskerAngle;
 		setWhiskerAngle(angle);
+		
+		float length = (velocity / maxVelocity) * maxRayLength;
+		length = Math.min(length + 1, maxRayLength);
+		setRayLength(length);
 		
 		return super.updateRays();
 	}
