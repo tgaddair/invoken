50a524bf1a6e2f541b138d1a9dca91802262c672
Travis Addair <tgaddair@gmail.com>
2015-02-01 14:57:53 -0800

    Improved Barrier quality score

--- InvokenGame/core/src/com/eldritch/invoken/actor/aug/Barrier.java
+++ InvokenGame/core/src/com/eldritch/invoken/actor/aug/Barrier.java
@@ -7,75 +7,91 @@ import com.eldritch.invoken.effects.Shield;
 import com.eldritch.invoken.encounter.Location;
 
 public class Barrier extends Augmentation {
-	private static class Holder {
+    private static class Holder {
         private static final Barrier INSTANCE = new Barrier();
-	}
-	
-	public static Barrier getInstance() {
-		return Holder.INSTANCE;
-	}
-	
+    }
+
+    public static Barrier getInstance() {
+        return Holder.INSTANCE;
+    }
+
     private Barrier() {
         super("barrier", false);
     }
-    
+
     @Override
     public void release(Agent owner) {
         owner.toggleOff(Shield.class);
     }
-    
-	@Override
-	public Action getAction(Agent owner, Agent target) {
-		return new ShieldAction(owner);
-	}
-	
-	@Override
+
+    @Override
+    public Action getAction(Agent owner, Agent target) {
+        return new ShieldAction(owner);
+    }
+
+    @Override
     public Action getAction(Agent owner, Vector2 target) {
         return new ShieldAction(owner);
     }
-	
-	@Override
-	public boolean isValid(Agent owner, Agent target) {
-		return true;
-	}
-	
-	@Override
+
+    @Override
+    public boolean isValid(Agent owner, Agent target) {
+        return true;
+    }
+
+    @Override
     public boolean isValid(Agent owner, Vector2 target) {
         return true;
     }
-	
-	@Override
+
+    @Override
     public int getCost(Agent owner) {
         return owner.isToggled(Shield.class) ? 0 : 5;
     }
-	
-	@Override
-    public float quality(Agent owner, Agent target, Location location) {
-	    if (target != null && target.getInventory().hasRangedWeapon()) {
-	        if (!owner.isToggled(Shield.class)) {
-	            return 2;
-	        }
-	    }
-	    
-	    // turn off the shield if not facing an enemy with a ranged weapon
-        return owner.isToggled(Shield.class) ? 2 : 0;
-    }
-	
-	public class ShieldAction extends AnimatedAction {
-		public ShieldAction(Agent actor) {
-			super(actor, Activity.Cast, Barrier.this);
-		}
 
-		@Override
-		public void apply(Location location) {
-			if (owner.toggle(Shield.class)) {
-				owner.addEffect(new Shield(owner, Barrier.this));
-			}
-		}
-		
-		@Override
+    @Override
+    public float quality(Agent owner, Agent target, Location location) {
+        if (!owner.isToggled(Shield.class)) {
+            // shield is currently inactive
+            if (owner.getInfo().getEnergyPercent() > 0.75f) {
+                // it's only worth using the shield if we have enough reserve energy to follow it
+                // up with an attack
+                // TODO: it would also help if we wished to do a flee or hide routine
+                for (Agent enemy : owner.getEnemies()) {
+                    if (enemy.getInventory().hasRangedWeapon()) {
+                        return 2;
+                    }
+                }
+            }
+            return 0;
+        } else {
+            for (Agent enemy : owner.getEnemies()) {
+                if (enemy.getInventory().hasRangedWeapon()) {
+                    // don't turn off the shield if we still have enemies shooting at us
+                    return 0;
+                }
+            }
+            
+            // turn off the shield if not facing an enemy with a ranged weapon
+            return 2;
+        }
+    }
+
+    public class ShieldAction extends AnimatedAction {
+        public ShieldAction(Agent actor) {
+            super(actor, Activity.Cast, Barrier.this);
+        }
+
+        @Override
+        public void apply(Location location) {
+            if (owner.toggle(Shield.class)) {
+                owner.addEffect(new Shield(owner, Barrier.this));
+            }
+        }
+
+        @Override
         public Vector2 getPosition() {
             return owner.getPosition();
         }
-	}
+    }
 }
