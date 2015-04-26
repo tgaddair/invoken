b425a39975d571b1b617cbdffcbc19955c0805c2
Travis Addair <tgaddair@gmail.com>
2015-04-26 13:15:18 -0700

    Added class for augmentation

---
+++ InvokenGame/core/src/com/eldritch/invoken/actor/aug/Mitosis.java
@@ -0,0 +1,85 @@
+package com.eldritch.invoken.actor.aug;
+
+import com.badlogic.gdx.math.Vector2;
+import com.eldritch.invoken.InvokenGame;
+import com.eldritch.invoken.actor.type.Agent;
+import com.eldritch.invoken.actor.type.Agent.Activity;
+import com.eldritch.invoken.actor.type.Npc;
+import com.eldritch.invoken.location.Location;
+import com.eldritch.invoken.util.SoundManager.SoundEffect;
+
+public class Mitosis extends Augmentation {
+    private static class Holder {
+        private static final Mitosis INSTANCE = new Mitosis();
+    }
+
+    public static Mitosis getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    private Mitosis() {
+        super(false);
+    }
+
+    @Override
+    public boolean isValid(Agent owner) {
+        // can only perform once
+        return !owner.isToggled(Mitosis.class);
+    }
+
+    @Override
+    public boolean isValid(Agent owner, Agent target) {
+        return isValid(owner);
+    }
+
+    @Override
+    public boolean isValid(Agent owner, Vector2 target) {
+        return isValid(owner);
+    }
+
+    @Override
+    public Action getAction(Agent owner, Agent target) {
+        return new MitosisAction(owner);
+    }
+
+    @Override
+    public Action getAction(Agent owner, Vector2 target) {
+        return new MitosisAction(owner);
+    }
+
+    @Override
+    public int getCost(Agent owner) {
+        return 2;
+    }
+
+    @Override
+    public float quality(Agent owner, Agent target, Location location) {
+        if (owner.getInfo().getHealth() < owner.getInfo().getMaxHealth()) {
+            return 5;
+        }
+        return 0;
+    }
+
+    public class MitosisAction extends AnimatedAction {
+        public MitosisAction(Agent actor) {
+            super(actor, Activity.Cast, Mitosis.this);
+        }
+
+        @Override
+        public void apply(Location location) {
+            owner.toggleOn(Mitosis.class);
+            
+            // every split reduces the max health of the twin
+            Npc twin = location.createNpc(owner.getInfo().getId(), owner.getPosition());
+            twin.getInfo().setMaxHealth(owner.getInfo().getHealth());
+            twin.getInfo().resetHealth();
+            
+            InvokenGame.SOUND_MANAGER.playAtPoint(SoundEffect.BUFF, owner.getPosition());
+        }
+
+        @Override
+        public Vector2 getPosition() {
+            return owner.getPosition();
+        }
+    }
+}
