cd4f18dfc05c87bd6e568aac7d7639f2cba6a3f1
Travis Addair <tgaddair@gmail.com>
2015-05-03 13:32:16 -0700

    Added augmentations to upload menu

--- InvokenGame/core/src/com/eldritch/invoken/ui/UploadMenu.java
+++ InvokenGame/core/src/com/eldritch/invoken/ui/UploadMenu.java
@@ -1,7 +1,9 @@
 package com.eldritch.invoken.ui;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -9,25 +11,24 @@ import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
-import com.eldritch.invoken.InvokenGame;
-import com.eldritch.invoken.actor.AgentInventory;
+import com.eldritch.invoken.actor.AgentInfo;
 import com.eldritch.invoken.actor.Profession;
-import com.eldritch.invoken.actor.items.Item;
+import com.eldritch.invoken.actor.aug.Augmentation;
+import com.eldritch.invoken.actor.items.Fragment;
 import com.eldritch.invoken.actor.type.Player;
 import com.eldritch.invoken.location.Location;
 import com.eldritch.invoken.proto.Disciplines.Discipline;
 import com.eldritch.invoken.screens.GameScreen;
-import com.eldritch.invoken.state.Inventory.ItemState;
 import com.eldritch.invoken.util.DefaultInputListener;
 import com.eldritch.invoken.util.Settings;
-import com.eldritch.invoken.util.SoundManager.SoundEffect;
 
 public class UploadMenu implements HudElement {
     private final Table container;
     private final Table table;
     private final Skin skin;
     private final Player player;
+    
+    private final Set<Augmentation> preparedAugs = new HashSet<>();
 
     private boolean active = false;
 
@@ -83,25 +84,80 @@ public class UploadMenu implements HudElement {
     private void refresh() {
         table.clear();
         
+        // init prepared augs
+        preparedAugs.clear();
+        for (Augmentation aug : player.getInfo().getAugmentations().getAugmentations()) {
+            preparedAugs.add(aug);
+        }
+
         Table topTable = new Table(skin);
         topTable.top();
-        
-        Label disciplineLabel = new Label("Disciplines", skin);
-        topTable.add(disciplineLabel).left().expandX().fillX().space(10);
-        topTable.row();
+
+        // level and fragments
+        {
+            int level = player.getInfo().getLevel();
+            Label levelLabel = new Label("Level: " + level, skin);
+            topTable.add(levelLabel).left().expandX().fillX().space(25);
+            topTable.row();
+
+            int currentFragments = player.getInventory().getItemCount(Fragment.getInstance());
+            int requiredFragments = AgentInfo.getFragmentRequirement(level + 1);
+            String fragmentsText = String.format("%d / %d", currentFragments, requiredFragments);
+            Label fragmentsLabel = new Label(fragmentsText, skin);
+            fragmentsLabel.setColor(Color.GRAY);
+            topTable.add(fragmentsLabel).left().expandX().fillX().space(10);
+            topTable.row();
+        }
 
         // disciplines
-        int i = 0;
-        Table disciplinesTable = new Table(skin);
-        for (Discipline d : Discipline.values()) {
-            Table disciplineTable = createTable(d);
-            disciplinesTable.add(disciplineTable).fillX().expandX();
-            if (++i % 2 == 0) {
-                disciplinesTable.row();
+        {
+            Label disciplineLabel = new Label("Disciplines", skin);
+            topTable.add(disciplineLabel).left().expandX().fillX().space(25);
+            topTable.row();
+
+            int i = 0;
+            Table disciplinesTable = new Table(skin);
+            for (Discipline d : Discipline.values()) {
+                Table disciplineTable = createTable(d);
+                disciplinesTable.add(disciplineTable).fillX().expandX();
+                if (++i % 2 == 0) {
+                    disciplinesTable.row();
+                }
             }
+            topTable.add(disciplinesTable).expandX().fillX().space(10);
+            topTable.row();
         }
-        topTable.add(disciplinesTable).expandX().fillX().space(10);
-        
+
+        // augmentations
+        {
+            Label augLabel = new Label("Augmentations", skin);
+            topTable.add(augLabel).left().expandX().fillX().space(25);
+            topTable.row();
+
+            int i = 0;
+            Table augTable = new Table(skin);
+            for (final Augmentation aug : player.getInfo().getAugmentations().getAugmentations()) {
+                final Image image = new Image(aug.getIcon());
+                image.addListener(new DefaultInputListener() {
+                    @Override
+                    public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                        toggle(aug, image);
+                    }
+                });
+                
+                // initial color
+                image.setColor(preparedAugs.contains(aug) ? Color.WHITE : Color.GRAY);
+                
+                augTable.add(image).size(48, 48).space(50);
+                if (++i % 5 == 0) {
+                    augTable.row();
+                }
+            }
+            topTable.add(augTable).expandX().fillX().space(10);
+            topTable.row();
+        }
+
+        // add the top table
         table.add(topTable).expand().fill();
         table.row();
 
@@ -109,6 +165,16 @@ public class UploadMenu implements HudElement {
         table.bottom().right();
         TextButton start = createCommitButton();
         table.add(start).size(200, 50).right();
+    }
+    
+    private void toggle(Augmentation aug, Image image) {
+        if (preparedAugs.contains(aug)) {
+            preparedAugs.remove(aug);
+            image.setColor(Color.GRAY);
+        } else {
+            preparedAugs.add(aug);
+            image.setColor(Color.WHITE);
+        }
     }
 
     private Table createTable(Discipline d) {
@@ -136,7 +202,7 @@ public class UploadMenu implements HudElement {
                 button.setText(String.valueOf(value));
             }
         });
-        table.add(button).size(50, 50).space(10);
+        table.add(button).size(50, 50).space(25);
 
         return table;
     }
