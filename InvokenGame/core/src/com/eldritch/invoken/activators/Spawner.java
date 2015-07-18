fe456ba52c8b43000b443d3df16e70894202ffe1
Travis Addair <tgaddair@gmail.com>
2015-07-18 16:30:07 -0700

    Fixed alignment of spawner

--- InvokenGame/core/src/com/eldritch/invoken/activators/Spawner.java
+++ InvokenGame/core/src/com/eldritch/invoken/activators/Spawner.java
@@ -20,50 +20,51 @@ import com.eldritch.invoken.util.Damage;
 import com.eldritch.invoken.util.Damager;
 import com.eldritch.invoken.util.Settings;
 
-public class Spawner extends CollisionActivator {
+public class Spawner extends CollisionActivator implements Damageable {
     private static final TextureRegion[] regions = GameScreen.getMergedRegion(GameScreen.ATLAS
             .findRegion("activators/spawner").split(32, 32));
-    
+
     private final SpawnerHandler handler;
-    
     private HealthBar healthBar = null;
 
     public Spawner(NaturalVector2 position) {
         super(position);
         handler = new SpawnerHandler();
     }
-    
+
     @Override
     public void register(Level level) {
         super.register(level);
         this.healthBar = level.createHealthBar();
     }
-    
+
     @Override
     public void render(float delta, OrthogonalTiledMapRenderer renderer) {
         TextureRegion frame = handler.getHealth() > 0 ? regions[0] : regions[1];
         Vector2 position = getPosition();
+        float w = frame.getRegionWidth() * Settings.SCALE;
+        float h = frame.getRegionHeight() * Settings.SCALE;
 
         Batch batch = renderer.getBatch();
         batch.begin();
-        batch.draw(frame, position.x, position.y, frame.getRegionWidth() * Settings.SCALE,
-                frame.getRegionHeight() * Settings.SCALE);
+        batch.draw(frame, position.x - w / 2, position.y - h / 2, w, h);
         batch.end();
-        
-//        if (handler.isDamaged() && isAlive()) {
-//            // update and render health
-//            healthBar.update(this);
-//            healthBar.draw(getLevel().getCamera());
-//        }
+
+        if (handler.isDamaged() && isAlive()) {
+            // update and render health
+            healthBar.update(this);
+            healthBar.draw(getLevel().getCamera());
+        }
     }
 
     @Override
     protected AgentHandler getCollisionHandler(InanimateEntity entity) {
         // NOTE: in order for this to be called, the TMX must have a TRANSIENT collider in the
         // collision layer and a "statics" or "dynamics" layer
+        getPosition().set(entity.getPhysicalPosition());
         return handler.with(entity);
     }
-    
+
     private void destroy() {
         for (InanimateEntity entity : handler.entities) {
             entity.finish();
@@ -73,14 +74,14 @@ public class Spawner extends CollisionActivator {
     private class SpawnerHandler extends DamageHandler {
         private static final float BASE_HEALTH = 25f;
         private float health = BASE_HEALTH;
-        
+
         private final List<InanimateEntity> entities = new ArrayList<>();
-        
+
         public SpawnerHandler with(InanimateEntity entity) {
             entities.add(entity);
             return this;
         }
-        
+
         public boolean isDamaged() {
             return getHealth() < getBaseHealth();
         }
@@ -104,24 +105,24 @@ public class Spawner extends CollisionActivator {
         }
     }
 
-//    @Override
-//    public float getBaseHealth() {
-//        return handler.getBaseHealth();
-//    }
-//
-//    @Override
-//    public float getHealth() {
-//        return handler.getHealth();
-//    }
-//
-//    @Override
-//    public boolean isAlive() {
-//        return handler.getHealth() > 0;
-//    }
-//
-//    @Override
-//    public void setHealthIndicator(Vector3 worldCoords) {
-//        Vector2 position = getPosition();
-//        worldCoords.set(position.x, position.y + 1, 0);
-//    }
+    @Override
+    public float getBaseHealth() {
+        return handler.getBaseHealth();
+    }
+
+    @Override
+    public float getHealth() {
+        return handler.getHealth();
+    }
+
+    @Override
+    public boolean isAlive() {
+        return handler.getHealth() > 0;
+    }
+
+    @Override
+    public void setHealthIndicator(Vector3 worldCoords) {
+        Vector2 position = getPosition();
+        worldCoords.set(position.x, position.y + 1, 0);
+    }
 }
