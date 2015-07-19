e30da65f3137f3244edcce882fe6aa6b0dab490e
Travis Addair <tgaddair@gmail.com>
2015-07-19 16:01:47 -0700

    Added vending machine so players can exchange fragments for items when resources are low

---
+++ InvokenGame/core/src/com/eldritch/invoken/activators/VendingMachine.java
@@ -0,0 +1,36 @@
+package com.eldritch.invoken.activators;
+
+import java.util.ArrayList;
+
+import com.eldritch.invoken.actor.type.Agent;
+import com.eldritch.invoken.actor.util.Lootable;
+import com.eldritch.invoken.location.NaturalVector2;
+import com.eldritch.invoken.proto.Actors.InventoryItem;
+import com.eldritch.invoken.state.Inventory;
+
+public class VendingMachine extends InteractableActivator implements Lootable {
+    private final Inventory inventory;
+
+    public VendingMachine(NaturalVector2 position) {
+        this(position, new Inventory(new ArrayList<InventoryItem>()));
+    }
+
+    public VendingMachine(NaturalVector2 position, Inventory inventory) {
+        super(position, 1, 2);
+        this.inventory = inventory;
+    }
+
+    @Override
+    protected void onBeginInteraction(Agent interactor) {
+        interactor.beginLooting(this);
+    }
+
+    @Override
+    protected void onEndInteraction(Agent interactor) {
+    }
+
+    @Override
+    public Inventory getInventory() {
+        return inventory;
+    }
+}
