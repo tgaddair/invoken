c9241b91b1efed7edb356c01c8be5e272c3000cb
Travis Addair <tgaddair@gmail.com>
2015-07-21 08:16:11 -0700

    Fixed the security station

--- InvokenGame/core/src/com/eldritch/invoken/activators/CameraHub.java
+++ InvokenGame/core/src/com/eldritch/invoken/activators/CameraHub.java
@@ -1,24 +1,14 @@
 package com.eldritch.invoken.activators;
 
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.eldritch.invoken.actor.aug.Crack;
 import com.eldritch.invoken.actor.type.Agent;
 import com.eldritch.invoken.location.Level;
 import com.eldritch.invoken.location.NaturalVector2;
 import com.eldritch.invoken.screens.GameScreen;
 
-public class CameraHub extends ClickActivator {
+public class CameraHub extends InteractableActivator {
 	public CameraHub(NaturalVector2 position) {
-		super(position);
-	}
-
-	@Override
-	public void activate(Agent agent, Level level) {
-		if (agent.isToggled(Crack.class) && level.hasSecurityCamera()) {
-			agent.setCamera(level.getFirstSecurityCamera());
-		} else {
-		    GameScreen.toast("Requires: Crack Augmentation");
-		}
+		super(position, 2, 2);
 	}
 
 	@Override
@@ -28,5 +18,19 @@ public class CameraHub extends ClickActivator {
     @Override
     public void render(float delta, OrthogonalTiledMapRenderer renderer) {
         // delegated to layer
+    }
+
+    @Override
+    protected void onBeginInteraction(Agent interactor) {
+        Level level = interactor.getLocation();
+        if (level.hasSecurityCamera()) {
+            interactor.setCamera(level.getFirstSecurityCamera());
+        } else {
+            GameScreen.toast("Security System Offline");
+        }
+    }
+
+    @Override
+    protected void onEndInteraction(Agent interactor) {
     }
 }
