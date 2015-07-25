3c221b4a31c83ca42a4688114bad1af50aa7c073
Travis Addair <tgaddair@gmail.com>
2015-07-25 09:19:50 -0700

    Added a penalty term to the movement cost between nodes based on the number of connections to bias paths towards the centers of rooms and halls, and away from edges where NPCs are likely to get stuck

--- InvokenGame/core/src/com/eldritch/invoken/actor/pathfinding/LocationConnection.java
+++ InvokenGame/core/src/com/eldritch/invoken/actor/pathfinding/LocationConnection.java
@@ -11,7 +11,9 @@ public class LocationConnection extends DefaultConnection<LocationNode> {
 
     @Override
     public float getCost() {
-        return getToNode().position.x != getFromNode().position.x
+        // scale down the cost with respect to the number of total connections
+        float baseCost = getToNode().position.x != getFromNode().position.x
                 && getToNode().position.y != getFromNode().position.y ? DIAGONAL_COST : 1;
+        return baseCost * Math.max(9 - fromNode.getConnections().size, 1);
     }
 }
