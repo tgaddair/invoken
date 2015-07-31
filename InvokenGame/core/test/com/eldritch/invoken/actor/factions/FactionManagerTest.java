0ab7acf9b1f2068f9f30e3c3dab6f87544a2668c
Travis Addair <tgaddair@gmail.com>
2015-07-31 07:09:24 -0700

    Added unit test for factions

---
+++ InvokenGame/core/test/com/eldritch/invoken/actor/factions/FactionManagerTest.java
@@ -0,0 +1,13 @@
+package com.eldritch.invoken.actor.factions;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FactionManagerTest {
+    @Test
+    public void thisAlwaysPasses() {
+        assertTrue(true);
+    }
+}
